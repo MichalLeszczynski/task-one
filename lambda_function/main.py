009ca0d04f5504f88ffd5460076643563bb86582
MichalLeszczynski <ext-michal.leszczynski@finago.com>
2022-06-07 08:32:49 +0200

    Working solution with dynamodb action and state management

--- lambda_function/main.py
+++ lambda_function/main.py
@@ -1,41 +1,46 @@
 import boto3
 import jinja2
 import json
+import datetime
+import os
+import time
 
+cfn = boto3.client("cloudformation")
 
 def launch_stack(stack_name, launch_params):
     cfn = boto3.client("cloudformation")
     capabilities = ["CAPABILITY_NAMED_IAM"]
     stackdata = "Empty"
     try:
-        print(f"Stackname: {stack_name}")
+        print(f"Creating stack: {stack_name}")
         stackdata = cfn.create_stack(
             StackName=stack_name,
             DisableRollback=True,
             TemplateBody=render_template(
-                launch_params.get("template_bucket_name"),
                 launch_params.get("template_filename"),
                 launch_params.get("template_params"),
             ),
             Capabilities=capabilities,
         )
+        stack_id = get_stack_id_by_name(stack_name)
     except Exception as e:
         print(str(e))
-    return stackdata
+    return stackdata, stack_id
 
 
 def delete_stack(stack_name):
     cfn = boto3.client("cloudformation")
+    stack_id = get_stack_id_by_name(stack_name)
     stackdata = "Empty"
     try:
-        print(f"Stackname: {stack_name}")
+        print(f"Deleting stack: {stack_name}")
         stackdata = cfn.delete_stack(StackName=stack_name)
     except Exception as e:
         print(str(e))
-    return stackdata
+    return stackdata, stack_id
 
 
-def render_template(bucket_name, template_filename, template_params) -> str:
+def render_template(template_filename, template_params) -> str:
     # template = download_file_from_s3(bucket_name, template_filename)
     template = read_template_file(template_filename)
     rendered_template = (
@@ -57,29 +62,113 @@ def read_template_file(file_name):
         stack = f.read()
     return stack
 
+def get_stack_id_by_name(stack_name):
+    return cfn.describe_stacks(StackName=stack_name).get('Stacks')[0].get('StackId')
 
-def handler(event, context):
+def wait_for_stack_to_finish(stack_id, time_elapsed=0):
+    stack_info = cfn.describe_stacks(StackName=stack_id)
+    stack_status = stack_info['Stacks'][0].get('StackStatus')
+    if stack_status.endswith("IN_PROGRESS"):
+        print(f"Waiting for stack to finish processing... Time elapsed: {time_elapsed}s")
+        time.sleep(10)
+        return wait_for_stack_to_finish(stack_id, time_elapsed+10)
+    else:
+        return stack_status
+
+def process_stream(record_values):
+
+    stream_result = "Failed"
+    print(f"Record: {record_values}")
+    stack_name = record_values.get("Stackname").get("S")
+    print(f"Stackname: {stack_name}")
+    timestamp = record_values.get("Timestamp").get("S")
+    print(f"Timestamp: {timestamp}")
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ['ActionsDynamoDbTableName'])
+    # getting value from db, as it's stripped of type values that mess with the data
+    response = table.get_item(
+        Key={"Stackname": stack_name, "Timestamp": timestamp}
+    )
+    print(response)
+    record = response.get("Item")
+
+    action = record.get("Action")
+    print(f"Action: {action}")
+    launch_params = record.get("LaunchParams")
+    print(f"LaunchParams: {launch_params}")
+
+    if action == "Create":
+        stream_result, stack_id = launch_stack(
+            stack_name,
+            launch_params,
+        )
+    elif action == "Delete":
+        stream_result, stack_id = delete_stack(stack_name)
+    else:
+        stream_result = "No action specified - aborting"
+        stack_id = None
+
+    if stack_id is not None:
+        stack_status = wait_for_stack_to_finish(stack_id)
+
+    print(stream_result)
+    print(stack_status)
+
+    db_entry = {
+        "Stackname": stack_name,
+        "Status": stack_status,
+        "Timestamp": datetime.datetime.now().isoformat(),
+    }
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ["StatesDynamoDbTableName"])
+    request_result = table.put_item(Item=db_entry)
+
+    return request_result
+
+def request_handler(event, context):
     print(f"Received event: {event}")
-    stack_result = "Uninitialized"
+    request_result = "Empty"
     try:
         body = json.loads(event.get("body"))
         action = body.get("action")
         print(f"Action: {action}")
-        stack_name = body.get("stack_name", "task-one-stack")
+        stack_name = body.get("stack_name", "task-two-stack")
         launch_params = body.get("launch_params")
+        timestamp = datetime.datetime.now().isoformat()
 
-        if action == "Create":
-            stack_result = launch_stack(
-                stack_name,
-                launch_params,
-            )
+        db_entry = {
+            "Stackname": stack_name,
+            "Timestamp": timestamp,
+            "Action": action,
+            "LaunchParams": launch_params,
+        }
 
-        elif action == "Delete":
-            print(f"Deleting stack: {stack_name}")
-            stack_result = delete_stack(stack_name)
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(os.environ["ActionsDynamoDbTableName"])
+        request_result = table.put_item(Item=db_entry)
+    except Exception as err:
+        print(str(err))
+    finally:
+        print(request_result)
+        return request_result
 
-        else:
-            stack_result = "No action specified - aborting"
+
+def stream_handler(event, context):
+    print(f"Received event: {event}")
+    stack_result = "Uninitialized"
+
+    try:
+        print(type(event))
+        print(type(event.get("Records")))
+        # records = json.loads(event.get("Records"))
+        records = event.get("Records")
+        for record in records:
+            print(type(record))
+            print(record.keys())
+            if record.get("eventName") == "INSERT":
+                stack_result = process_stream(record.get("dynamodb").get("NewImage"))
     except Exception as err:
         print(str(err))
     finally:
--- makefile
+++ makefile
@@ -1,7 +1,11 @@
 
-STACKNAME=mlesz-lambda-stack
-BUCKETNAME=mlesz-cloudformation-templates
+STACKNAME=mlesz-lambda-stack-t2
+BUCKETNAME=mlesz-cloudformation-templates-t2
 REGION=eu-central-1
+DEPLOYED_INTERNAL_STACK=mlesz-ec2-stack
+
+validate:
+	aws cloudformation validate-template --template-body file://external-stack.yaml
 
 deploy: package
 	aws cloudformation deploy --template-file ./packaged-stack.yaml --stack-name $(STACKNAME) --capabilities CAPABILITY_NAMED_IAM
@@ -34,14 +38,14 @@ lambda_url_output:
 	aws cloudformation describe-stacks --query 'Stacks[?StackName==`$(STACKNAME)`].Outputs[0][?OutputKey==`FunctionUrl`].OutputValue' --output=text > url.txt # hackish output indexing
 
 payload_key_id_output:
-	aws cloudformation describe-stacks --query 'Stacks[?StackName==`mlesz-ec2-stack`].Outputs[0][?OutputKey==`KeyPairId`].OutputValue' --output text > key_pair_id.txt # hackish output indexing
+	aws cloudformation describe-stacks --query 'Stacks[?StackName==`$(DEPLOYED_INTERNAL_STACK)`].Outputs[0][?OutputKey==`KeyPairId`].OutputValue' --output text > key_pair_id.txt # hackish output indexing
 
 get-keys: payload_key_id_output
 	echo "`aws ssm get-parameter --name /ec2/keypair/$(shell cat key_pair_id.txt) --region eu-central-1 --with-decryption | jq .Parameter.Value | cut -d '\"' -f 2`" > key.pem
 	chmod 600 key.pem
 
 payload_public_dns_output:
-	aws cloudformation describe-stacks --query 'Stacks[?StackName==`mlesz-ec2-stack`].Outputs[1][?OutputKey==`InstanceDnsName`].OutputValue' --output text > public_dns.txt
+	aws cloudformation describe-stacks --query 'Stacks[?StackName==`$(DEPLOYED_INTERNAL_STACK)`].Outputs[1][?OutputKey==`InstanceDnsName`].OutputValue' --output text > public_dns.txt
 
 login: get-keys payload_public_dns_output
 	ssh -i ./key.pem ec2-user@$(shell cat public_dns.txt)
